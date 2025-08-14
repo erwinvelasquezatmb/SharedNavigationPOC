be725a8abb7b7840db80635effcdc0b6c73e7cb2
joseph <joseph@anytimemailbox.com>
2025-08-14 14:21:03 +0800

    initial commit

---
+++ Anytime.Mailbox.Shared.Navigation/wwwroot/js/navigation.js
@@ -0,0 +1,15 @@
+﻿function loadNavigationBar(target, model) {
+
+    if ($(target) === 0) {
+        console.error('target not found');
+        return;
+    }
+
+    let navHtml = `
+        <nav class="navbar navbar-light bg-white border-bottom px-3">
+            <div class="container-fluid">
+    `;
+
+    //Logo
+
+}
